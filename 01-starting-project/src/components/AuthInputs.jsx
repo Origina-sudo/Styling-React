c6133c08e89270e1c614c1cf5bb4934ecc004ab3
Benjamin Arthur <arthurba8585@gmail.com>
2024-06-18 17:28:14 +0000

    Added a lil something something

--- 01-starting-project/src/components/AuthInputs.jsx
+++ 01-starting-project/src/components/AuthInputs.jsx
@@ -21,9 +21,9 @@ const Input = styled.input`
 width: 100%;
   padding: 0.75rem 1rem;
   line-height: 1.5;
-  background-color: #d1d5db;
-  color: #374151;
-  border: 1px solid transparent;
+  background-color: ${({invalid})=> invalid ? '#fed2d2' : '#d1d5db'};
+  color: ${({invalid})=> invalid ? '#ef4444' : '#374151' };
+  border: 1px solid  ${({invalid})=> invalid ? '#f73f3f' : 'transparent'};
   border-radius: 0.25rem;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
 `
@@ -55,12 +55,12 @@ export default function AuthInputs() {
           <Label invalid={emailNotValid} >Email</Label>
           <Input
             type="email"
-            className={emailNotValid ? 'invalid' : undefined}
+            // className={emailNotValid ? 'invalid' : undefined}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
         </p>
         <p>
-          <Label >Password</Label>
+          <Label invalid={passwordNotValid}  >Password</Label>
           <Input
             type="password"
             // className={passwordNotValid ? 'invalid' : undefined}
