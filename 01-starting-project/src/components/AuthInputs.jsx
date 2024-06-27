f17f40f8dbc5cdad7bc429aa1e9f6282b9b103f2
Benjamin Arthur <arthurba8585@gmail.com>
2024-06-27 17:29:26 +0000

    About to start components combinations

--- 01-starting-project/src/components/AuthInputs.jsx
+++ 01-starting-project/src/components/AuthInputs.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
  import {styled} from 'styled-components';
+import Button from './Button';
+import  Input from './input';
 
  const controldiv = styled.div`
   display: flex;
@@ -7,38 +9,8 @@ import { useState } from 'react';
    gap: 0.5rem;
    margin-bottom: 1.5rem;
 `
-const Label = styled.label`
-  display: block;
-  margin-bottom: 0.5rem;
-  font-size: 0.75rem;
-  font-weight: 700;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  color:${({$invalid})=> $invalid ? '#f87171' : '#6b7280'} ;
-`;
 
-const Input = styled.input`
-width: 100%;
-  padding: 0.75rem 1rem;
-  line-height: 1.5;
-  background-color: ${({$invalid})=> $invalid ? '#fed2d2' : '#d1d5db'};
-  color: ${({$invalid})=> $invalid ? '#ef4444' : '#374151' };
-  border: 1px solid  ${({$invalid})=> $invalid ? '#f73f3f' : 'transparent'};
-  border-radius: 0.25rem;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`
-const Button = styled.button`
- padding: 1rem 2rem;
-  font-weight: 600;
-  text-transform: uppercase;
-  border-radius: 0.25rem;
-  color: #1f2937;
-  background-color: #f0b322;
-  border-radius: 6px;
-  border: none;
-  & :hover
 
-`
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
   const [enteredPassword, setEnteredPassword] = useState('');
@@ -62,17 +34,15 @@ export default function AuthInputs() {
   return (
     <div id="auth-inputs">
       <controldiv>
-        <p className='paragraph'>
-          <Label $invalid={emailNotValid}>Email</Label>
+       
           <Input
           $invalid={emailNotValid}
             type="email"
             // className={emailNotValid ? 'invalid' : undefined}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
-        </p>
-        <p>
-          <Label $invalid={passwordNotValid}  >Password</Label>
+         
+       
           <Input
           $invalid={passwordNotValid}
             type="password"
@@ -81,13 +51,13 @@ export default function AuthInputs() {
               handleInputChange('password', event.target.value)
             }
           />
-        </p>
+        
       </controldiv>
       <div className="actions">
         <button type="button" className="text-button">
           Create a new account
         </button>
-        <button className='button' onClick={handleLogin}>Sign In</button>
+        <Button   onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );
---
+++ 01-starting-project/src/components/Button.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+
+ const Button = styled.button`
+ padding: 1rem 2rem;
+  font-weight: 600;
+  text-transform: uppercase;
+  border-radius: 0.25rem;
+  color: #1f2937;
+  background-color: #f0b322;
+  border-radius: 6px;
+  border: none;
+  & :hover
+
+`;
+
+export default Button;
