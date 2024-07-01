84f0106c5a54e4f49ab31f744b3bd8cbce7b92c1
Benjamin Arthur <arthurba8585@gmail.com>
2024-07-01 14:00:28 +0000

    new

--- 01-starting-project/src/components/Header.jsx
+++ 01-starting-project/src/components/Header.jsx
@@ -5,7 +5,7 @@ import logo from '../assets/logo.png';
 
 export default function Header() {
   return (
-    <header className=''>
+    <header className='flex-col items-center mt-8 mb-16'>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
       <p  >A community of artists and art-lovers.</p>
